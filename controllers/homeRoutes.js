29ff863bc86c5e100237d318f61a1e71d15442e6
George Huliaris <georgehuliaris@me.com>
2021-04-16 18:19:48 -0500

    added more boiler plate stuff

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,32 @@
+const router = require("express").Router();
+const { POST, USER, Comment } = require("..model");
+const withAuth = require('../utils/auth');
+
+// borrowed from mini project so make sure you change this stuff to match!!!!
+//seriously! dont forget!
+router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const projectData = await Project.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const projects = projectData.map((project) => project.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        projects, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
